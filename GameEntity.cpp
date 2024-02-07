9cad8499323066ec041759beb497488205ab867e
Jabrecia Washington <jabwashi2@gmail.com>
2024-02-07 01:02:53 -0500

    defined functions in GameEntity.cpp

--- GameEntity.cpp
+++ GameEntity.cpp
@@ -1,1 +1,19 @@
 #include "GameEntity.h"
+
+GameEntity::GameEntity(std::shared_ptr<Mesh> mesh_ptr)
+{
+}
+
+std::shared_ptr<Mesh> GameEntity::GetMesh()
+{
+    return std::shared_ptr<Mesh>();
+}
+
+void GameEntity::SetMesh()
+{
+}
+
+std::shared_ptr<Transform> GameEntity::GetTransform()
+{
+    return std::shared_ptr<Transform>();
+}
