84186d413a495c05736da4ecc6fec11a29d193ba
Jabrecia Washington <jabwashi2@gmail.com>
2024-02-08 21:10:33 -0500

    created BufferStructs.h

---
+++ BufferStructs.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <DirectXMath.h>
+
+struct VertexShaderExternalData
+{
+	// alignment is good!
+	//DirectX::XMFLOAT4 colorTint;
+	DirectX::XMFLOAT4X4 worldMatrix;
+	DirectX::XMFLOAT4X4 viewMatrix;
+	DirectX::XMFLOAT4X4 projMatrix;
+};
