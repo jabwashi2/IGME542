9eedf639e8de4e96349a3f17cc0e4a9c86d10722
jabwashi2 <jabwashi2@gmail.com>
2024-02-20 15:08:50 -0500

    started material getters and setters; need to finish other methods

--- Material.cpp
+++ Material.cpp
@@ -1,1 +1,72 @@
 #include "Material.h"
+
+Material::Material(Microsoft::WRL::ComPtr<ID3D12PipelineState> _pipelineState, DirectX::XMFLOAT3 _colorTint, DirectX::XMFLOAT2 _uvScale, DirectX::XMFLOAT2 _uvOffset)
+{
+	// save passed in values
+	this->pipelineState = _pipelineState;
+	this->colorTint = _colorTint;
+	this->uvScale = _uvScale;
+	this->uvOffset = _uvOffset;
+
+	// initialize other values
+	this->finalized = false;
+}
+
+Material::~Material()
+{
+}
+
+void Material::AddTexture(D3D12_CPU_DESCRIPTOR_HANDLE srv, int slot)
+{
+}
+
+void Material::FinalizeMaterial()
+{
+}
+
+
+// Getters
+
+DirectX::XMFLOAT3 Material::GetColorTint()
+{
+	return colorTint;
+}
+
+DirectX::XMFLOAT2 Material::GetUVScale()
+{
+	return uvScale;
+}
+
+DirectX::XMFLOAT2 Material::GetUVOffset()
+{
+	return uvOffset;
+}
+
+Microsoft::WRL::ComPtr<ID3D12PipelineState> Material::GetPipeLineState()
+{
+	return pipelineState;
+}
+
+D3D12_GPU_DESCRIPTOR_HANDLE Material::GetFinalGPUHandleForSRVs()
+{
+	return finalGPUHandleForSRVs;
+}
+
+
+// Setters
+
+void Material::SetColorTint()
+{
+}
+
+void Material::SetUVScale()
+{
+}
+
+void Material::SetUVOffset()
+{
+}
+
+void Material::SetPipelineState()
+{
+}
--- Material.h
+++ Material.h
@@ -6,12 +6,35 @@
 class Material
 {
 public:
+	Material(Microsoft::WRL::ComPtr<ID3D12PipelineState> _pipelineState, DirectX::XMFLOAT3 _colorTint, DirectX::XMFLOAT2 _uvScale, DirectX::XMFLOAT2 _uvOffset);
+	~Material();
+
+	void AddTexture(D3D12_CPU_DESCRIPTOR_HANDLE srv, int slot);
+	void FinalizeMaterial();
+
+	// getters
+	DirectX::XMFLOAT3 GetColorTint();
+	DirectX::XMFLOAT2 GetUVScale();
+	DirectX::XMFLOAT2 GetUVOffset();
+	Microsoft::WRL::ComPtr<ID3D12PipelineState> GetPipeLineState();
+	D3D12_GPU_DESCRIPTOR_HANDLE GetFinalGPUHandleForSRVs();
+
+	// setters
+	void SetColorTint();
+	void SetUVScale();
+	void SetUVOffset();
+	void SetPipelineState();
 
 private:
+	bool finalized;
+
 	DirectX::XMFLOAT3 colorTint;
 	DirectX::XMFLOAT2 uvScale;
 	DirectX::XMFLOAT2 uvOffset;
-	bool finalized;
+
 	Microsoft::WRL::ComPtr<ID3D12PipelineState> pipelineState;
+
+	D3D12_CPU_DESCRIPTOR_HANDLE textureSRVsBySlot[4];
+	D3D12_GPU_DESCRIPTOR_HANDLE finalGPUHandleForSRVs;
 };
 
