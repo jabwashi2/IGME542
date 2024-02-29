5d9d1f62dc4dde7703d07e11fd7ef682b5e80e73
jabwashi2 <jabwashi2@gmail.com>
2024-02-29 14:43:04 -0500

    hittable object list created

--- Raytracing/OfflineRaytracing/OfflineRaytracing/hittable.h
+++ Raytracing/OfflineRaytracing/OfflineRaytracing/hittable.h
@@ -15,6 +15,7 @@ public:
     void set_face_normal(const ray& r, const vec3& outward_normal) {
         // Sets the hit record normal vector.
         // NOTE: the parameter `outward_normal` is assumed to have unit length.
+        // we're seeing if the ray is going against ours
 
         front_face = dot(r.direction(), outward_normal) < 0;
         normal = front_face ? outward_normal : -outward_normal;
