c38392874a81bf9302de38d32010ddf6441c21e4
Jabrecia Washington <jabwashi2@gmail.com>
2024-02-29 23:05:03 -0500

    lambertian reflection

--- Raytracing/OfflineRaytracing/OfflineRaytracing/camera.h
+++ Raytracing/OfflineRaytracing/OfflineRaytracing/camera.h
@@ -100,7 +100,7 @@ private:
             return color(0, 0, 0);
 
         if (world.hit(r, interval(0.001, infinity), rec)) {
-            vec3 direction = random_on_hemisphere(rec.normal);
+            vec3 direction = rec.normal + random_unit_vector();
             return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
         }
 
