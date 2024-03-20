16f25d983cc72996afa9fb4c3e5ed26bffe77c2f
Jabrecia Washington <jabwashi2@gmail.com>
2024-03-20 00:19:11 -0400

    updates to DX12Helper (for RaytracingHelper compatibility)

--- Raytracing/Real Time Raytracing/DX12Helper.h
+++ Raytracing/Real Time Raytracing/DX12Helper.h
@@ -42,6 +42,19 @@ private:
 public:
 	~DX12Helper();
 
+	Microsoft::WRL::ComPtr<ID3D12Resource> CreateBuffer(
+		UINT64 size,
+		D3D12_HEAP_TYPE heapType = D3D12_HEAP_TYPE_DEFAULT,
+		D3D12_RESOURCE_STATES state = D3D12_RESOURCE_STATE_COMMON,
+		D3D12_RESOURCE_FLAGS flags = D3D12_RESOURCE_FLAG_NONE,
+		UINT64 alignment = 0);
+
+	void ReserveSrvUavDescriptorHeapSlot(
+		D3D12_CPU_DESCRIPTOR_HANDLE* reservedCPUHandle,
+		D3D12_GPU_DESCRIPTOR_HANDLE* reservedGPUHandle);
+
+	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> GetDefaultAllocator();
+
 	// Initialization for singleton
 	void Initialize(
 		Microsoft::WRL::ComPtr<ID3D12Device> device,
